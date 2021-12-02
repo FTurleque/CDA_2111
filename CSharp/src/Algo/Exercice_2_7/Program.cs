15de120b911732194c1fd30e26377a5f8e8fd083
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-12-02 16:54:04 +0100

    End exercice 2.7

--- CSharp/src/Algo/Exercice_2_7/Program.cs
+++ CSharp/src/Algo/Exercice_2_7/Program.cs
@@ -1,31 +1,33 @@
 ﻿using System;
+using System.Text;
 
 namespace Exercice_2_7
 {
     class Program
     {
         private static string userEntry;
+        private static int[] ageOfPeople = new int[20];
+        private static int adultsNumber = 1;
+        private static int minorNumber = 1;
+        private static StringBuilder ageOfAllPeoples;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }    
-        private static int AskNumber(string message)
+            AskNumber("Entrez l'âge de 20 personnes \nPour la personne n°");
+            Console.WriteLine(CheckIfTheyAreAdult());
+            Console.WriteLine(ageOfAllPeoples);
+        }
+
+        private static void AskNumber(string message)
         {
-            for (int i = 0; i < 21; i++)
+            for (int i = 0; i < 20; i++)
             {
-                Console.WriteLine($"Entrez l'a");
+                Console.WriteLine(message + (i + 1) + " :");
 
                 userEntry = Console.ReadLine();
                 try
                 {
-                    int tmp = int.Parse(userEntry);
-
-                    if (tmp < 3 && tmp >= 0) return tmp;
-                    else
-                    {
-                        throw new FormatException("Vous n'avez pas entrez un chiffre entre 0 et 2, recommencez :");
-                    }
+                    ageOfPeople[i] = int.Parse(userEntry);
                 }
                 catch (FormatException)
                 {
@@ -34,7 +36,28 @@ namespace Exercice_2_7
             }
         }
 
+        private static string CheckIfTheyAreAdult()
+        {
+            ageOfAllPeoples = new StringBuilder("");
+            for (int i = 0; i < ageOfPeople.Length - 1; i++)
+            {
+                int tmp = ageOfPeople[i];
+                if (tmp < 18)
+                {
+                    minorNumber++;
+                }
+                else
+                {
+                    adultsNumber++;
+                }
+                ageOfAllPeoples.Append(tmp);
+                ageOfAllPeoples.Append(" ");
+            }
+
+            if (minorNumber == ageOfPeople.Length) return "Il n'y a pas d'adultes !";
+            else if (adultsNumber == ageOfPeople.Length) return "Il n'y a pas de jeunes !";
+            else return "Il y a des jeunes et des adultes !";
+
+        }
     }
-
-
 }
