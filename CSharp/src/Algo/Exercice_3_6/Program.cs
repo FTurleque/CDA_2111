6789705ec18a12eabe65b66c42bf00db2c402895
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-12-07 15:01:38 +0100

    End Exercice 3.6

--- CSharp/src/Algo/Exercice_3_6/Program.cs
+++ CSharp/src/Algo/Exercice_3_6/Program.cs
@@ -8,7 +8,7 @@ namespace Exercice_3_6
     {
         static void Main(string[] args)
         {
-            char[] wordToFind = AskAWord("Jeu du pendu !\nEntrez le mot Mystère ne comportant que 6 lettres différentes :");
+            char[] wordToFind = AskAWord("Jeu du pendu !\nEntrez le mot Mystère comportant minimum 5 lettres :");
             Console.Clear();
             char[] wordHidden = new char[wordToFind.Length];
             wordHidden = WordLetterPlaced(wordHidden);
@@ -16,8 +16,6 @@ namespace Exercice_3_6
             Console.WriteLine(FindAWord(wordToFind, wordHidden));
         }
 
-        // Vérification si le mot comporte au maximum 6 charatères different
-        // A insérer
         private static char[] AskAWord(string message)
         {
             Console.WriteLine(message);
@@ -26,8 +24,11 @@ namespace Exercice_3_6
                 string userEntry = Console.ReadLine();
                 try
                 {
-                    if (Regex.IsMatch(userEntry, $"^[a-zA-Z]{6}")) return userEntry.ToCharArray();
-                    /*else if ()*/
+                    if (Regex.IsMatch(userEntry, $"^[a-zA-Z]*$"))
+                    {
+                        if (userEntry.Length > 4) return userEntry.ToCharArray();
+                        else throw new Exception("Vous n'avez pas entrez assez de charachtères, recommencez avec au moins 5 lettre differentes :");
+                    }
                     else throw new Exception("Vous n'avez pas entrez un mot !\nRéessayez :");
                 }
                 catch (Exception e)
@@ -57,14 +58,12 @@ namespace Exercice_3_6
                 {
                     Console.WriteLine(e.Message);
                 }
-
-
             } while (true);
-
         }
 
         private static char[] WordLetterPlaced(char[] _word)
         {
+            
             for (int i = 0; i < _word.Length; i++)
             {
                 _word[i] = '-';
@@ -82,15 +81,14 @@ namespace Exercice_3_6
             return dysplayWord;
         }
 
-
-
         private static string FindAWord(char[] _wordToFind, char[] _wordHidden)
         {
-            int find = 0;
-            int life = 0;
+            int wordFind = 0;
+            int life = 6;
             do
             {
-                char suggestedLetter = AskLetter("Entrez une lettre de l'alphabet :");
+                bool find = false;
+                char suggestedLetter = AskLetter($"Vous avez encore {life} Entrez une lettre de l'alphabet :");
                 Console.Clear();
                 for (int i = 0; i < _wordToFind.Length; i++)
                 {
@@ -98,14 +96,17 @@ namespace Exercice_3_6
                     if (tmp == suggestedLetter)
                     {
                         _wordHidden[i] = suggestedLetter;
-                        find++;
+                        wordFind++;
+                        find = true;
                     }
                 }
                 Console.WriteLine(DisplayWord(_wordHidden));
-                life++;
-            } while (!(life == 6 || find == _wordToFind.Length));
+                if (find == false) life--;
+                if (life > 1) Console.WriteLine($"Il vous reste {life} chances !");
+                else Console.WriteLine($"Il vous reste {life} chance !");
+            } while (!(life == 0 || wordFind == _wordToFind.Length));
 
-            if (find == _wordToFind.Length) return "Vous avez gagnez, bravo !";
+            if (wordFind == _wordToFind.Length) return "Vous avez gagnez, bravo !";
             else return "Vous avez perdu ! :( ";
         }
 
