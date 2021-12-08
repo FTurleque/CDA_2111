94584483c1695d6250bba023fc5f26ff7d8475ff
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-12-08 16:43:46 +0100

    End Exercice 2.2.2

---
+++ CSharp/src/Algo/Recursif_Exercice_2_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursif_Exercice_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aNumber = AskNumber("Algorithme récursifs sur les nombres\n\nEntrez un nombre entier pour 'a' :");
+            int bNumber = AskNumber("Algorithme récursifs sur les nombres\n\nEntrez un nombre entier 'b' :");
+        }
+
+        private static int AskNumber(string message)
+        {
+            Console.WriteLine(message);
+            do
+            {
+                string userEntry = Console.ReadLine();
+                try
+                {
+                    return int.Parse(userEntry);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Vous n'avez pas entrez un nombre entier positif, recommencez :");
+                }
+            } while (true);
+        }
+    }
+}
