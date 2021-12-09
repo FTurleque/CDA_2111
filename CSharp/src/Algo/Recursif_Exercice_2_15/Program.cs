431d6b909cf6a7d9c2acfc4997f11f7878c4d9a5
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2021-12-09 20:46:32 +0100

    End recursif exercice 2.15

---
+++ CSharp/src/Algo/Recursif_Exercice_2_15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Recursif_Exercice_2_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Somme des éléments d'un tableau\n\n");
+            int[] sumArrayNumber = { 15, 23, 70, 80, 2, 9, 50 };
+            int index = 0;
+            Console.WriteLine($"La somme du tableau est de : {GetArraySum(sumArrayNumber, index)} !");
+        }
+
+        private static int GetArraySum(int[] _sumArrayNumber, int _index)
+        {
+            if (_sumArrayNumber.Length - 1 == _index) return _sumArrayNumber[_index];
+            return _sumArrayNumber[_index] + GetArraySum(_sumArrayNumber, _index + 1);
+        }
+    }
+}
