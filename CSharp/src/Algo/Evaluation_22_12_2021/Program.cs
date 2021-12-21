57bf6395f33ff1b5d09319f7a4b746467552208a
fturleque <84319762+FTurleque@users.noreply.github.com>
2021-12-21 16:46:12 +0100

    Ajout pour l'exam

---
+++ CSharp/src/Algo/Evaluation_22_12_2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Evaluation_22_12_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
