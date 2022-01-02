1061c34eb1a23427222c2e39c613cf49e6f0a731
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2022-01-02 21:07:37 +0100

    Starting POO

---
+++ POO/CSharp/src/object_oriented/Account/Client.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class Client
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        /// <summary>
+        /// Client constructor
+        /// </summary>
+        /// <param name="_firstName">First name of the client</param>
+        /// <param name="_lastName">Last name of the client</param>
+        /// <param name="_age">Age of the client</param>
+        public Client(string _firstName, string _lastName, int _age)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+        }
+
+
+
+        /// <summary>
+        /// The client can make a payement
+        /// </summary>
+        /// <param name="message">Name of payement strore</param>
+        /// <param name="_payment">Amount of money spent</param>
+        public void MakeAPayment(string message, double _payment)
+        {
+            
+        }
+
+        /// <summary>
+        /// The client can make a withdrawal in a bank
+        /// </summary>
+        /// <param name="_withdrawal">Withdrawal amount</param>
+        public void MakeACashWithdrawal(double _withdrawal)
+        {
+            
+        }
+
+        /// <summary>
+        /// The client can make a deposit in his account
+        /// </summary>
+        /// <param name="message">Name of the deposit</param>
+        /// <param name="_deposit">Deposit amount</param>
+        public void MakeADeposit(string message, double _deposit)
+        {
+            
+        }
+
+        /*private Account AccountCreation(Client _client, double _accountNB)
+        {
+            List<Account> accounts = new();
+            int accountID;
+            do
+            {
+                accountID = RandomNumber(1, 20);
+            } while (accountNumber.Contains(accountID));
+            accountNumber.Add(accountID);
+            Account accountClient = new(_client, accountID, _accountNB);
+            accounts.Add(accountClient);
+            return accounts;
+        }
+
+        private int RandomNumber(int start, int end)
+        {
+            Random number = new Random();
+            return number.Next(start, end);
+        }*/
+
+    }
+}
---
+++ POO/CSharp/src/object_oriented/Account/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Account
+{
+    class Program
+    {
+        private static List<Client> clients = new();
+        private static List<Account> accountsClients = new();
+        private List<int> accountNumber = new();
+
+        static void Main(string[] args)
+        {
+            Client c1 = new("Fabrice", "Turleque", 30);
+            Account c1Account = new(c1, 1, 400);
+            accountsClients.Add(c1Account);
+
+            Client c2 = new("Cyril", "Legrand", 35);
+            /*List<Account> c2Account = AccountCreation(c2, 300);
+            accountsClients.Add(c2Account);*/
+
+            Client c3 = new("Axel", "Bouiller", 18);
+            /*List<Account> c3Account = AccountCreation(c3, 100);
+            accountsClients.Add(c3Account);*/
+
+            Client c4 = new("Damien", "Simon", 25);
+            /*List<Account> c4Account = AccountCreation(c4, 200);
+            accountsClients.Add(c4Account);*/
+
+            Client c5 = new("Xavier", "Bardon", 24);
+            /*List<Account> c5Account = AccountCreation(c5, 15);
+            accountsClients.Add(c5Account);*/
+
+            Client c6 = new("Julien", "Milletre", 26);
+            /*List<Account> c6Account = AccountCreation(c6, 30);
+            accountsClients.Add(c6Account);
+
+            List<Account> c1Account2 = AccountCreation(c1, 1000);
+            accountsClients.Add(c1Account2);*/
+
+            clients.Add(c1);
+            clients.Add(c2);
+            clients.Add(c3);
+            clients.Add(c4);
+            clients.Add(c5);
+            clients.Add(c6);
+            Console.Write(clients);
+
+
+            Console.ReadKey();
+        }
+    }
+}
