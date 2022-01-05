5ef518efc1ee7fdca9bc547353a5ac253fa96234
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-01-05 16:44:32 +0100

    Creation app Geometrie

---
+++ POO/CSharp/src/object_oriented/Geometrie/Segment2D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrie
+{
+    class Segment2D
+    {
+        public Segment2D()
+        {
+
+        }
+
+        public Segment2D(Point2D p1, Point2D p2) 
+        { 
+
+        }
+    }
+}
