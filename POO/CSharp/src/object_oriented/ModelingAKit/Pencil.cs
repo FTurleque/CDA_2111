e25225a63659a2cb861d81972d43c7d319c9bd48
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-01-11 16:49:28 +0100

    Create ModelingAKit

---
+++ POO/CSharp/src/object_oriented/ModelingAKit/Pencil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingAKit
+{
+    abstract class Pencil : WritingTool
+    {
+        private bool _tipOfMineToCarve;
+
+        public bool TipOfMineToCarve
+        {
+            get { return _tipOfMineToCarve; }
+            set { _tipOfMineToCarve = value; }
+        }
+
+
+        public void Sharpen()
+        {
+
+        }
+
+        public override void Write()
+        {
+
+        }
+    }
+}
---
+++ POO/CSharp/src/object_oriented/ModelingAKit/WritingTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingAKit
+{
+    abstract class WritingTool : ITool
+    {
+        private string _color;
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        private int _writingCapacity;
+
+        public int WritingCapacity
+        {
+            get { return _writingCapacity; }
+        }
+
+        public virtual void Write()
+        {
+
+        }
+    }
+}
