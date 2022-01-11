ff84d2f18e930e1d29805a4c2c89c159b06ecee9
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-01-11 16:49:28 +0100

    Create ModelingAKit

---
+++ POO/CSharp/src/object_oriented/ModelingAKit/Kit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingAKit
+{
+    abstract class Kit
+    {
+        private int _volume;
+
+        public int Volume
+        {
+            get { return _volume; }
+            private set { _volume = value; }
+        }
+
+        private bool _isOpen;
+
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            private set { _isOpen = value; }
+        }
+
+        public Kit(int volume)
+        {
+            Volume = volume;
+        }
+
+        public void Open()
+        {
+
+        }
+
+        public void Closed()
+        {
+
+        }
+    }
+}
