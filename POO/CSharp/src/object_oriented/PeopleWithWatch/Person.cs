2e824091c0f812f184111e896a415bf5bfbe50fe
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2022-01-20 21:40:39 +0100

    Add People with watch

---
+++ POO/CSharp/src/object_oriented/PeopleWithWatch/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleWithWatch
+{
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private Watch _theWatch;
+
+        public Person(string firstname, string lastName)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastName;
+        }
+
+        public Watch TheWatch
+        {
+            get { return _theWatch; }
+            set { _theWatch = value; }
+        }
+        
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public void GiveAWatch(Person personToGiveTheWatch)
+        {
+            if (personToGiveTheWatch.TheWatch == null) Console.WriteLine("You don't have a watch, you can't give it away.");
+            else
+            {
+                personToGiveTheWatch.TheWatch = this.TheWatch;
+                this.TheWatch = null;
+            }
+        }
+
+        public void AddWatch(Watch theWatch)
+        {
+            this.TheWatch = theWatch;
+        }
+
+        public string GivingTheTime()
+        {
+            return this.TheWatch.Time.ToString("t", new System.Globalization.CultureInfo("fr-FR"));
+        }
+    }
+}
---
+++ POO/CSharp/src/object_oriented/PeopleWithWatch/Watch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleWithWatch
+{
+    class Watch
+    {
+        private DateTime _time;
+        private int _hour;
+        private int _minute;
+
+        public Watch(DateTime time)
+        {
+            this.Time = time;
+        }
+
+        public Watch(int hour, int minute)
+        {
+            this.Time = MakeTimeCreation(hour, minute);
+        }
+
+        public Watch(Watch otherWath)
+        {
+            this.Time = MakeTimeCreation(otherWath.Hour, otherWath.Minute);
+        }
+
+        private DateTime MakeTimeCreation(int hour, int minute)
+        {
+            DateTime tmp = new DateTime(hour / minute);
+            return tmp;
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set { _minute = value; }
+        }
+
+        public int Hour
+        {
+            get { return _hour; }
+            set { _hour = value; }
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+        public string AdvanceTheTime(int timeToAdvance)
+        {
+            Hour = this.Time.Hour;
+            Minute = this.Time.Minute;
+            for (int i = timeToAdvance; i > 0; i--)
+            {
+                if (Minute == 59)
+                {
+                    Hour += 1;
+                    Minute = 0;
+                }
+                else Minute += 1;
+            }
+            return $"{Hour:D2}:{Minute:D2}";
+        }
+    }
+}
