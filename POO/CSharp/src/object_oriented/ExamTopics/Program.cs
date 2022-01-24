64bbd08f02d1ef097370c7a605d609fddfc686a4
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2022-01-24 19:51:12 +0100

    Exam Topic Creation

---
+++ POO/CSharp/src/object_oriented/ExamTopics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamTopics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
