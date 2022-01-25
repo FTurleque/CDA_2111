478dc6daa1783248a4b78fe995b53bcad09ff0a4
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-01-25 16:07:37 +0100

    Update Exam Topics

--- POO/CSharp/src/object_oriented/ExamTopics/Answer.cs
+++ POO/CSharp/src/object_oriented/ExamTopics/Answer.cs
@@ -8,5 +8,47 @@ namespace ExamTopics
 {
     class Answer
     {
+        /*private string _answerToQuestion;
+        private bool _goodAnswer;*/
+        private Dictionary<string, bool> _answersToQuestion;
+        private Question _questionAsk;
+
+        public Answer(Question questionAsk)
+        {
+            this.QuestionAsk = questionAsk;
+            this.AnswersToQuestion = new();
+        }
+
+        public Dictionary<string, bool> AnswersToQuestion
+        {
+            get { return _answersToQuestion; }
+            set { _answersToQuestion = value; }
+        }
+
+        /*public bool GoodAnswer
+        {
+            get { return _goodAnswer; }
+            set { _goodAnswer = value; }
+        }
+
+        public string AnswerToQuestion
+        {
+            get { return _answerToQuestion; }
+            set { _answerToQuestion = value; }
+        }*/
+
+        public Question QuestionAsk
+        {
+            get { return _questionAsk; }
+            init { _questionAsk = value; }
+        }
+
+        public void AddAnswer(string answerToQuestion, bool goodAnswer)
+        {
+            for (int i = 0; i < QuestionAsk.MultipleChoice; i++)
+            {
+                AnswersToQuestion.Add(answerToQuestion, goodAnswer);
+            }
+        }
     }
 }
---
+++ POO/CSharp/src/object_oriented/ExamTopics/Question.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamTopics
+{
+    class Question
+    {
+        private string _statementOfReview;
+        private int _difficulty;
+        // Not need is the array.Length.
+        private int _multipleChoice;
+        private double _numberOfPointsPerQuestion;
+        private Answer _answers;
+
+        public Question(string statementOfReview, double numberOfPointsPerQuestion, int multipleChoice, int difficulty)
+        {
+            this.StatementOfReview = statementOfReview;
+            this.NumberOfPointsPerQuestion = numberOfPointsPerQuestion;
+            this.MultipleChoice = multipleChoice;
+            this.Difficulty = difficulty;
+            this.Answers = null;
+        }
+
+        public Answer Answers
+        {
+            get { return _answers; }
+            set { _answers = value; }
+        }
+
+        public double NumberOfPointsPerQuestion
+        {
+            get { return _numberOfPointsPerQuestion; }
+            init { _numberOfPointsPerQuestion = value; }
+        }
+
+        public int MultipleChoice
+        {
+            get { return _multipleChoice; }
+            init { _multipleChoice = value; }
+        }
+
+        public int Difficulty
+        {
+            get { return _difficulty; }
+            private set { _difficulty = value; }
+        }
+
+        public string StatementOfReview
+        {
+            get { return _statementOfReview; }
+            init { _statementOfReview = value; }
+        }
+    }
+}
