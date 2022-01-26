4bc93603b81fd95847c90afbc8e39b6b22149941
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-01-26 16:50:41 +0100

    Start ONF

---
+++ POO/CSharp/src/object_oriented/ONF/Parcel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONF
+{
+    class Parcel
+    {
+        private uint _id;
+        private List<Tree> _trees;
+        private List<GpsCoordonate> _coordonates;
+
+        public Parcel(uint _id, List<GpsCoordonate> coordonates)
+        {
+            this.ParcelId = _id;
+            this.Coordonates = coordonates;
+            this.Trees = null;
+        }
+
+        public List<GpsCoordonate> Coordonates
+        {
+            get => this._coordonates;
+            set 
+            {
+                if (_coordonates.Count == 4)
+                {
+                    _coordonates = value;
+                }
+            }
+        }
+
+        public uint ParcelId
+        {
+            get => this._id;
+            set => this._id = value;
+        }
+
+        public List<Tree> Trees
+        {
+            get => this._trees;
+            set => this._trees = value;
+        }
+
+        public int GetNumberTrees()
+        {
+            return this.Trees.Count;
+        }
+
+        public void PlantATree(Tree tree)
+        {
+            this.Trees.Add(tree);
+        }
+
+        public float GetSurface()
+        {
+            return 0;
+        }
+    }
+}
