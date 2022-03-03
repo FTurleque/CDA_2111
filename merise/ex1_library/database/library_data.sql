043deb9656b3b11d0dc76654258cbdd7e4f236a3
FTurleque <fabrice.turleque@gmail.com>
2022-03-03 17:02:08 +0100

    Update library

--- merise/ex1_library/database/library_data.sql
+++ merise/ex1_library/database/library_data.sql
@@ -1,35 +1,51 @@
 USE db_library;
-
 GO
 
-INSERT INTO Customers (customer_firstname, customer_lastname, customer_street_nb, customer_street, customer_postal_code, customer_city, customer_deposit)
-VALUES	('Fabrice', 'Turleque', 10, 'Chemin du petit pont', 69200, 'Mulhouse', 100),
-		('Nicholas', 'Dupuis', 200, 'Boulevard des nations', 69200, 'Mulhouse', 100),
-		('Gabrielle', 'Alonso', 4, 'Place général DE Gaulle', 69100, 'Mulhouse', 100),
-		('Galahad', 'Bornad', 250, 'Rue de Camelot', 69150, 'Mulhouse', 100),
-		('Jim', 'Kurk', 1, 'Chemin de l Enterprise', 69200, 'Mulhouse', 100),
-		('Lorenzo', 'Lamas', 150, 'Boulevard de Belcourt', 69050, 'Mulhouse', 100),
-		('Duncan', 'McLeod', 19, 'Place du chavalier sans tête', 69100, 'Mulhouse', 100),
-		('Conor', 'McLeod', 50, 'Rue de Grande Bretagne', 69150, 'Mulhouse', 100)
+IF NOT EXISTS (SELECT * FROM Customers)
+BEGIN
+	INSERT INTO Customers (customer_firstname, customer_lastname, customer_street_nb, customer_street, customer_postal_code, customer_city, customer_deposit)
+	VALUES	('Fabrice', 'Turleque', 10, 'Chemin du petit pont', 69200, 'Mulhouse', 100),
+			('Nicholas', 'Dupuis', 200, 'Boulevard des nations', 69200, 'Mulhouse', 100),
+			('Gabrielle', 'Alonso', 4, 'Place général DE Gaulle', 69100, 'Mulhouse', 100),
+			('Galahad', 'Bornad', 250, 'Rue de Camelot', 69150, 'Mulhouse', 100),
+			('Jim', 'Kurk', 1, 'Chemin de l Enterprise', 69200, 'Mulhouse', 100),
+			('Lorenzo', 'Lamas', 150, 'Boulevard de Belcourt', 69050, 'Mulhouse', 100),
+			('Duncan', 'McLeod', 19, 'Place du chavalier sans tête', 69100, 'Mulhouse', 100),
+			('Conor', 'McLeod', 50, 'Rue de Grande Bretagne', 69150, 'Mulhouse', 100)
+END
 
-INSERT INTO Collection_points (collection_point_name)
-VALUES ('Onion'), ('Gaston'), ('Primarium'), ('Gargamel'), ('Veritas')
+IF NOT EXISTS (SELECT * FROM Collection_points)
+BEGIN
+	INSERT INTO Collection_points (collection_point_name)
+	VALUES ('Onion'), ('Gaston'), ('Primarium'), ('Gargamel'), ('Veritas')
+END
 
-INSERT INTO States (state_condition)
-VALUES ('Très mauvais'), ('Mauvais'), ('Passable'), ('Bon'), ('Très bon'), ('Comme neuf'), ('Neuf')
+IF NOT EXISTS (SELECT * FROM States)
+BEGIN
+	INSERT INTO States (state_condition)
+	VALUES ('Très mauvais'), ('Mauvais'), ('Passable'), ('Bon'), ('Très bon'), ('Comme neuf'), ('Neuf')
+END
 
-INSERT INTO Writers (writer_firstname, writer_lastname)
-VALUES ('Stephen', 'King'), ('Frank', 'Herbert'), ('J.K.', 'Rowling'), ('Stephenie', 'Meyer'), ('William', 'Shakespeare')
+IF NOT EXISTS (SELECT * FROM Writers)
+BEGIN
+	INSERT INTO Writers (writer_firstname, writer_lastname)
+	VALUES ('Stephen', 'King'), ('Frank', 'Herbert'), ('J.K.', 'Rowling'), ('Stephenie', 'Meyer'), ('William', 'Shakespeare')
+END
 
-INSERT INTO Publishers (publisher_name)
-VALUES ('Gallimard'), ('Albin Michel'), ('Livre de poche'), ('J ais lu')
+IF NOT EXISTS (SELECT * FROM Publishers)
+BEGIN
+	INSERT INTO Publishers (publisher_name)
+	VALUES ('Gallimard'), ('Albin Michel'), ('Livre de poche'), ('J ais lu')
+END
 
-INSERT INTO Books (book_isbn, book_title, book_writer_id, book_publisher_id, book_state_id, book_collection_point_id, book_purchasedate)
-VALUES	('7895SK', 'Carrie', 1, 1, 5, 2, '2022-09-15'), ('654SK', 'Cujo', 1, 2, 4, 1, '2000-10-01'), ('664SK', 'Le Fléau', 1, 2, 6, 3, '1999-05-21'),
-		('785FH', 'Dune', 2, 4, 7, 5, '2002-02-22'), ('624FH', 'Cujo', 2, 3, 1, 1, '2007-12-07'), ('664FH', 'Le Fléau', 2, 4, 2, 3, '1989-07-30'),
-		('5548JKR', 'Harry Potter 1', 3, 2, 4, 2, '2022-09-15'), ('8524JKR', 'Harry Potter 2', 3, 2, 5, 1, '2000-10-01'), ('1234JKR', 'Harry Potter 7', 3, 2, 7, 3, '1999-05-21'),
-		('548SM', 'Twilight 1', 4, 1, 4, 4, '2004-02-18'), ('824SM', 'Twilight 2', 4, 2, 5, 1, '2017-12-25'), ('657SM', 'Twilight 5', 4, 4, 4, 5, '2008-06-21')
-
+IF NOT EXISTS (SELECT * FROM Books)
+BEGIN
+	INSERT INTO Books (book_isbn, book_title, book_writer_id, book_publisher_id, book_state_id, book_collection_point_id, book_purchasedate)
+	VALUES	('7895SK', 'Carrie', 1, 1, 5, 2, '2022-09-15'), ('654SK', 'Cujo', 1, 2, 4, 1, '2000-10-01'), ('664SK', 'Le Fléau', 1, 2, 6, 3, '1999-05-21'),
+			('785FH', 'Dune', 2, 4, 7, 5, '2002-02-22'), ('624FH', 'Cujo', 2, 3, 1, 1, '2007-12-07'), ('664FH', 'Le Fléau', 2, 4, 2, 3, '1989-07-30'),
+			('5548JKR', 'Harry Potter 1', 3, 2, 4, 2, '2022-09-15'), ('8524JKR', 'Harry Potter 2', 3, 2, 5, 1, '2000-10-01'), ('1234JKR', 'Harry Potter 7', 3, 2, 7, 3, '1999-05-21'),
+			('548SM', 'Twilight 1', 4, 1, 4, 4, '2004-02-18'), ('824SM', 'Twilight 2', 4, 2, 5, 1, '2017-12-25'), ('657SM', 'Twilight 5', 4, 4, 4, 5, '2008-06-21')
+END
 
 /*
 SELECT * FROM Books;
--- merise/ex1_library/database/library_structure.sql
+++ merise/ex1_library/database/library_structure.sql
@@ -5,62 +5,82 @@ END
 GO
 
 USE db_library;
-
 GO
 
-CREATE TABLE Customers
-(customer_id INT NOT NULL IDENTITY,
-customer_firstname VARCHAR(50) NOT NULL,
-customer_lastname VARCHAR(50) NOT NULL,
-customer_street_nb INT NOT NULL,
-customer_street VARCHAR(255) NOT NULL,
-customer_postal_code INT NOT NULL,
-customer_city VARCHAR(50),
-customer_deposit INT NOT NULL,
-CONSTRAINT PK_customer_id PRIMARY KEY (customer_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Customers' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Customers
+	(customer_id INT NOT NULL IDENTITY,
+	customer_firstname VARCHAR(50) NOT NULL,
+	customer_lastname VARCHAR(50) NOT NULL,
+	customer_street_nb INT NOT NULL,
+	customer_street VARCHAR(255) NOT NULL,
+	customer_postal_code INT NOT NULL,
+	customer_city VARCHAR(50),
+	customer_deposit INT NOT NULL,
+	CONSTRAINT PK_customer_id PRIMARY KEY (customer_id));
+END
 
-CREATE TABLE States
-(state_id INT NOT NULL IDENTITY,
-state_condition VARCHAR(30),
-CONSTRAINT PK_state_id PRIMARY KEY (state_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'States' AND xtype = 'U')
+BEGIN
+	CREATE TABLE States
+	(state_id INT NOT NULL IDENTITY,
+	state_condition VARCHAR(30),
+	CONSTRAINT PK_state_id PRIMARY KEY (state_id));
+END
 
-CREATE TABLE Collection_points
-(collection_point_id INT NOT NULL IDENTITY,
-collection_point_name VARCHAR(50),
-CONSTRAINT PK_collection_point_id PRIMARY KEY (collection_point_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Collection_points' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Collection_points
+	(collection_point_id INT NOT NULL IDENTITY,
+	collection_point_name VARCHAR(50),
+	CONSTRAINT PK_collection_point_id PRIMARY KEY (collection_point_id));
+END
 
-CREATE TABLE Writers
-(writer_id INT NOT NULL IDENTITY,
-writer_firstname VARCHAR(50) NOT NULL,
-writer_lastname VARCHAR(50) NOT NULL,
-CONSTRAINT PK_writer_id PRIMARY KEY (writer_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Writers' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Writers
+	(writer_id INT NOT NULL IDENTITY,
+	writer_firstname VARCHAR(50) NOT NULL,
+	writer_lastname VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_writer_id PRIMARY KEY (writer_id));
+END
 
-CREATE TABLE Publishers
-(publisher_id INT NOT NULL IDENTITY,
-publisher_name VARCHAR(50) NOT NULL,
-CONSTRAINT PK_publisher_id PRIMARY KEY (publisher_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Publishers' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Publishers
+	(publisher_id INT NOT NULL IDENTITY,
+	publisher_name VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_publisher_id PRIMARY KEY (publisher_id));
+END
 
-CREATE TABLE Borrows
-(borrow_id INT NOT NULL IDENTITY,
-borrow_date DATE NOT NULL,
-borrow_late BIT NOT NULL,
-borrow_number TINYINT NOT NULL,
-borrow_customer_id INT,
-CONSTRAINT PK_borrow_id PRIMARY KEY (borrow_id),
-CONSTRAINT FK_Borrows_customer_id FOREIGN KEY (borrow_customer_id) REFERENCES Customers (customer_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Borrows' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Borrows
+	(borrow_id INT NOT NULL IDENTITY,
+	borrow_date DATE NOT NULL,
+	borrow_late BIT NOT NULL,
+	borrow_number TINYINT NOT NULL,
+	borrow_customer_id INT,
+	CONSTRAINT PK_borrow_id PRIMARY KEY (borrow_id),
+	CONSTRAINT FK_Borrows_customer_id FOREIGN KEY (borrow_customer_id) REFERENCES Customers (customer_id));
+END
 
-CREATE TABLE Books
-(book_isbn VARCHAR(50) NOT NULL,
-book_title VARCHAR(64) NOT NULL,
-book_purchasedate DATE NOT NULL,
-book_state_id INT NOT NULL,
-book_writer_id INT NOT NULL,
-book_publisher_id INT NOT NULL,
-book_collection_point_id INT NOT NULL,
-book_borrow_id INT,
-CONSTRAINT PK_book_isbn PRIMARY KEY (book_isbn),
-CONSTRAINT FK_Books_state_id FOREIGN KEY (book_state_id) REFERENCES States (state_id),
-CONSTRAINT FK_Books_author_firstname FOREIGN KEY (book_writer_id) REFERENCES Writers (writer_id),
-CONSTRAINT FK_Book_publisher_id FOREIGN KEY (book_publisher_id) REFERENCES Publishers (publisher_id),
-CONSTRAINT FK_Books_collection_point_id FOREIGN KEY (book_collection_point_id) REFERENCES Collection_points (collection_point_id),
-CONSTRAINT FK_Books_borrow_id FOREIGN KEY (book_borrow_id) REFERENCES Borrows (borrow_id));
+IF NOT EXISTS (SELECT * FROM sys.sysobjects WHERE name = 'Books' AND xtype = 'U')
+BEGIN
+	CREATE TABLE Books
+	(book_isbn VARCHAR(50) NOT NULL,
+	book_title VARCHAR(64) NOT NULL,
+	book_purchasedate DATE NOT NULL,
+	book_state_id INT NOT NULL,
+	book_writer_id INT NOT NULL,
+	book_publisher_id INT NOT NULL,
+	book_collection_point_id INT NOT NULL,
+	book_borrow_id INT,
+	CONSTRAINT PK_book_isbn PRIMARY KEY (book_isbn),
+	CONSTRAINT FK_Books_state_id FOREIGN KEY (book_state_id) REFERENCES States (state_id),
+	CONSTRAINT FK_Books_author_firstname FOREIGN KEY (book_writer_id) REFERENCES Writers (writer_id),
+	CONSTRAINT FK_Book_publisher_id FOREIGN KEY (book_publisher_id) REFERENCES Publishers (publisher_id),
+	CONSTRAINT FK_Books_collection_point_id FOREIGN KEY (book_collection_point_id) REFERENCES Collection_points (collection_point_id),
+	CONSTRAINT FK_Books_borrow_id FOREIGN KEY (book_borrow_id) REFERENCES Borrows (borrow_id));
+END
