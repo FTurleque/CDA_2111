ece5b194d9e7e726dc2987d03fe5822eea31f1e1
fturleque <84319762+FTurleque@users.noreply.github.com>
2022-03-16 12:48:19 +0100

    Update db_travel_agency_procedure.sql

--- merise/travel_agency/database/db_travel_agency_procedure.sql
+++ merise/travel_agency/database/db_travel_agency_procedure.sql
@@ -44,7 +44,7 @@ BEGIN
 END
 GO
 
-EXEC Add_Trip 'NewToto', 100, '2020-05-01T12:20:00', '2020-05-08T12:20:00', 899, 'Découvrez tous les site les plus fantasique du Marrakech.', NULL, 5
+-- EXEC Add_Trip 'NewToto', 100, '2020-05-01T12:20:00', '2020-05-08T12:20:00', 899, 'Découvrez tous les site les plus fantasique du Marrakech.', NULL, 5
 
 SELECT * FROM Trips
 GO
@@ -58,23 +58,33 @@ BEGIN
 	SET NOCOUNT ON;
 	IF (EXISTS (SELECT client_id FROM Clients WHERE client_id = @client_id))
 	BEGIN
+		DECLARE @update_clients TABLE (
+				client_lastname VARCHAR(32),
+				client_firstname VARCHAR(32),
+				client_email VARCHAR(128),
+				client_phone VARCHAR(16),
+				client_added DATE,
+				client_password VARCHAR(60),
+				client_com_code INT);
+
 		UPDATE Clients
-			SET @field_to_update =
+			SET client_firstname =
 			(CASE 
 				WHEN @field_to_update = client_lastname THEN @data_to_update
-				WHEN @field_to_update = client_firstname THEN @data_to_update
+				WHEN @field_to_update = 'client_firstname' THEN @data_to_update
 				WHEN @field_to_update = client_email THEN @data_to_update
 				WHEN @field_to_update = client_phone THEN @data_to_update
-				WHEN CONVERT(@field_to_update AS date) = client_added THEN CAST(@data_to_update AS date)
 				WHEN @field_to_update = client_password THEN @data_to_update
-				WHEN @field_to_update = client_com_code THEN @data_to_update
+				WHEN @field_to_update = CAST(client_com_code AS VARCHAR) THEN @data_to_update
 				END)
+			OUTPUT deleted.client_firstname AS BeforeValue, @data_to_update AS AfterValue
+			WHERE client_id = @client_id
 		PRINT('Updated !')
 	END
 	ELSE
 		PRINT('Not updated !')
 END
 
-EXEC Update_Client 1, 'client_firstname', 'New Jean-Louis'
+EXEC Update_Client 1, 'client_firstname', 'Jean_Pierre'
 
 SELECT * FROM Clients
