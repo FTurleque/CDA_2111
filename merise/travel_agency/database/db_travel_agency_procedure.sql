aa4cfe2fd29b4a13a082854c217ddcab788bb8c6
FTurleque <fabrice.turleque@gmail.com>
2022-03-17 21:37:52 +0100

    Update db_travel_agency_procedure.sql

--- merise/travel_agency/database/db_travel_agency_procedure.sql
+++ merise/travel_agency/database/db_travel_agency_procedure.sql
@@ -48,43 +48,96 @@ GO
 
 SELECT * FROM Trips
 GO
-
+-- update of a single field 
 CREATE OR ALTER PROCEDURE Update_Client
 	@client_id INT,
-	@field_to_update VARCHAR(10),
+	@field_to_update VARCHAR(max),
 	@data_to_update VARCHAR(max)
 AS
 BEGIN
 	SET NOCOUNT ON;
 	IF (EXISTS (SELECT client_id FROM Clients WHERE client_id = @client_id))
 	BEGIN
-		DECLARE @update_clients TABLE (
-				client_lastname VARCHAR(32),
-				client_firstname VARCHAR(32),
-				client_email VARCHAR(128),
-				client_phone VARCHAR(16),
-				client_added DATE,
-				client_password VARCHAR(60),
-				client_com_code INT);
-
-		UPDATE Clients
-			SET client_firstname =
-			(CASE 
-				WHEN @field_to_update = client_lastname THEN @data_to_update
-				WHEN @field_to_update = 'client_firstname' THEN @data_to_update
-				WHEN @field_to_update = client_email THEN @data_to_update
-				WHEN @field_to_update = client_phone THEN @data_to_update
-				WHEN @field_to_update = client_password THEN @data_to_update
-				WHEN @field_to_update = CAST(client_com_code AS VARCHAR) THEN @data_to_update
-				END)
-			OUTPUT deleted.client_firstname AS BeforeValue, @data_to_update AS AfterValue
+		IF (@field_to_update = 'client_lastname')
+		BEGIN
+			UPDATE Clients
+			SET client_lastname = @data_to_update
 			WHERE client_id = @client_id
+		PRINT('Updated client_lastname !')
+		END
+		IF (@field_to_update = 'client_firstname')
+		BEGIN
+			UPDATE Clients
+			SET client_firstname = @data_to_update
+			WHERE client_id = @client_id
+		PRINT('Updated client_firstname !')
+		END
+		IF (@field_to_update = 'client_email')
+		BEGIN
+			UPDATE Clients
+			SET client_email = @data_to_update
+			WHERE client_id = @client_id
+		PRINT('Updated client_email !')
+		END
+		IF (@field_to_update = 'client_phone')
+		BEGIN
+			UPDATE Clients
+			SET client_phone = @data_to_update
+			WHERE client_id = @client_id
+		PRINT('Updated client_phone !')
+		END
+		IF (@field_to_update = 'client_password')
+		BEGIN
+			UPDATE Clients
+			SET client_password = @data_to_update
+			WHERE client_id = @client_id
+		PRINT('Updated client_password !')
+		END
+		IF (@field_to_update = 'client_com_code')
+		BEGIN
+			UPDATE Clients
+			SET client_com_code = @data_to_update
+			WHERE client_id = @client_id
+		PRINT('Updated client_com_code !')
+		END
+	END
+	ELSE
+		PRINT('Not updated !')
+END
+GO
+
+CREATE OR ALTER PROCEDURE All_Update_Client
+	@client_id INT,
+	@client_lastname VARCHAR(32),
+	@client_firstname VARCHAR(32),
+	@client_email VARCHAR(128),
+	@client_phone VARCHAR(16),
+	@client_added DATE,
+	@client_password VARCHAR(60),
+	@client_com_code INT NOT NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF (EXISTS (SELECT client_id FROM Clients WHERE client_id = @client_id))
+	BEGIN
+		UPDATE Clients
+			SET client_lastname = @client_lastname,
+				client_firstname = @client_firstname,
+				client_email = @client_email,
+				client_phone = @client_phone,
+				client_added = @client_added,
+				client_password = @client_password,
+				client_com_code = @client_com_code
+		WHERE client_id = @client_id
 		PRINT('Updated !')
 	END
 	ELSE
 		PRINT('Not updated !')
 END
+GO
 
-EXEC Update_Client 1, 'client_firstname', 'Jean_Pierre'
+
+
+EXEC Update_Client 1, 'client_firstname', 'Jean'
 
 SELECT * FROM Clients
