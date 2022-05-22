ac464dbbeb9d440dae70d0916757cc58cddc7aff
FTurleque <fabrice.turleque@gmail.com>
2022-05-22 11:15:44 +0200

    Extract MyAppStreaming_v2 to put in other project

--- web/backend/MyAppStreaming_v2/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-exports.index = (req, res) => {
-    console.log('In the controller')
-    res.send('Welcome home')
-}
-
-exports.about = (req, res) => {
-    console.log('In the controller')
-    res.send('About')
-}
