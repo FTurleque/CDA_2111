2e23683a61c81d1d4ab0161aa7bd062b72c7d283
FTurleque <fabrice.turleque@gmail.com>
2022-07-04 20:25:59 +0200

    Starting server web C#

---
+++ appDesktopCSharp/ThreadManagement/HttpServer/WebServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    public class WebServer
+    {
+        Boolean running;
+        HttpListener server;
+
+        public async void Start()
+        {
+            using (server = new HttpListener())
+            {
+                server.Prefixes.Add("http://localhost:8000/");
+                // server.Prefixes.Add(Environment.MachineName);
+                server.Start();
+                running = true;
+
+                while(running)
+                {
+                    var context = await server.GetContextAsync();
+                    Thread thread1 = new Thread(OnRequest);
+                    thread1.Start(context);
+                }
+            };
+        }
+
+        public void Stop()
+        {
+            running = false;
+        }
+
+        public void OnRequest(Object req)
+        {
+            if(req is HttpListenerContext ctx)
+            {
+                string s = "<html>Bonjour !</html>";
+                byte[] res = Encoding.UTF8.GetBytes(s.ToCharArray());
+
+                ctx.Response.Close(res, true);
+            }
+        }
+    }
+}
